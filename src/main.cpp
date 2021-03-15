a2283b346ad1ebdda272f25619979221c3efbde3
technarr <technarrus@gmail.com>
2021-03-15 14:22:30 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+
+/***************************************************
+ *УЗНАТЬ АДРЕС ДАТЧИКА Dallas В МОНИТОРЕ ПОРТА*
+ **************************************************/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+ 
+OneWire oneWire(D4); // вход датчиков 18b20
+DallasTemperature ds(&oneWire);
+
+DeviceAddress sensor0;
+
+void setup() {
+    Serial.begin(9600);
+    ds.begin();
+    
+    if (!ds.getAddress(sensor0, 0)){ 
+      Serial.println("Unable to find address for Device 0"); 
+    } 
+    
+    // отпаравляем адрес из массива в монитор порта
+    Serial.print("address sensor 0: ");   
+    for (uint8_t i = 0; i < 8; i++)  {  
+      Serial.print("0x");   
+      Serial.print(sensor0[i], HEX);
+      Serial.print(", ");
+    }
+}
+
+void loop() {
+
+}
